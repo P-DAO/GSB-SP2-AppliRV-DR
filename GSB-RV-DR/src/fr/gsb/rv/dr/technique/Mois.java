b43517d946ce3d9463a76db5ad6bdb7dec854f05
P-DAO <perrine.dao.sio@gmail.com>
2021-04-23 01:10:35 +0200

    Création des Mois

---
+++ GSB-RV-DR/src/fr/gsb/rv/dr/technique/Mois.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.gsb.rv.dr.technique;
+
+/**
+ *
+ * @author developpeur
+ */
+public enum Mois {
+        Janivier, Février, Mars, Avril, Mai, Juin, Juillet, Août, Septembre, Octobre, Novembre, Décembre
+    
+}
